5b52721b88206a56a1dd6dd016e31cd685c89b85
pushkar koppanathi <pkoppana@in.ibm.com>
2021-02-23 00:12:04 +0530

    SpringBoot, Spring Security and JWT

---
+++ src/main/java/com/kpr/jwt/JwtdemoApplication.java
@@ -0,0 +1,21 @@
+package com.kpr.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@SpringBootApplication
+public class JwtdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtdemoApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
---
+++ src/main/java/com/kpr/jwt/model/JwtResponse.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.kpr.jwt.model;
+
+/**
+ * @author PushkarRajuKoppanath
+ *
+ */
+public class JwtResponse {
+	
+	private String jwtToken;
+
+	public JwtResponse(String jwtToken) {
+		super();
+		this.jwtToken = jwtToken;
+	}
+
+	public JwtResponse() {
+	}
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+	
+	
+	
+}
